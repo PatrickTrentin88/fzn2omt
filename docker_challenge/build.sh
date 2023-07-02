6b38fe38f6b3057a4c6c8c7f97d0b16e66bf3ace
Patrick Trentin <patrick.trentin.88@gmail.com>
2023-07-02 13:33:31 -0500

    docker recipes for MiniZinc Challenge

---
+++ docker_challenge/build.sh
@@ -0,0 +1,5 @@
+docker build -t patricktrentin88/optimathsat_1-7-3_int_mznc2023 -f optimathsat_int .
+docker build -t patricktrentin88/optimathsat_1-7-3_bv_mznc2023 -f optimathsat_bv .
+
+docker push patricktrentin88/optimathsat_1-7-3_int_mznc2023
+docker push patricktrentin88/optimathsat_1-7-3_bv_mznc2023
