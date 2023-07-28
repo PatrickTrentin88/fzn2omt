1cb23756dd6a1bfc429cead21d899444983319ab
Patrick Trentin <patrick.trentin.88@gmail.com>
2023-07-27 21:47:53 -0500

    minor

--- docker_challenge/build.sh
+++ docker_challenge/build.sh
@@ -1,6 +1,6 @@
-docker build -t patricktrentin88/optimathsat_1-7-4_int_mznc2023 -f optimathsat_int .
-docker build -t patricktrentin88/optimathsat_1-7-4_int_smt2_mznc2023 -f optimathsat_int_smt2 .
-docker build -t patricktrentin88/optimathsat_1-7-4_bv_mznc2023 -f optimathsat_bv .
+docker build --no-cache -t patricktrentin88/optimathsat_1-7-4_int_mznc2023 -f optimathsat_int .
+docker build --no-cache -t patricktrentin88/optimathsat_1-7-4_int_smt2_mznc2023 -f optimathsat_int_smt2 .
+docker build --no-cache -t patricktrentin88/optimathsat_1-7-4_bv_mznc2023 -f optimathsat_bv .
 
 docker push patricktrentin88/optimathsat_1-7-4_int_mznc2023
 docker push patricktrentin88/optimathsat_1-7-4_int_smt2_mznc2023
